c44b51647be91511fa347cd951b98b74a8a63200
Gaurav <sql.gaurav@gmail.com>
2022-07-01 17:51:26 -0400

    removed unnecessary space

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/navbar'
+import "./App.css";
+import React from "react";
+import Navbar from "./components/navbar";
 function App() {
   return (
     <div>
